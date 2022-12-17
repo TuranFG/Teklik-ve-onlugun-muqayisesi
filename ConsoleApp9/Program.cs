cec78ad2212a47132f72b5419c176856af1db3ea
TuranFG <turanfg@code.edu.az>
2022-12-17 09:38:59 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int teklik = number % 10;
+            int onluq = (number - teklik) % 100 /10;
+
+            Console.WriteLine(teklik);
+            Console.WriteLine(onluq);
+
+            if (teklik > onluq) Console.WriteLine("teklik onluqdan boyukdur");
+            else Console.WriteLine("onluq teklikden boyukdur");
+        }
+    }
+}
